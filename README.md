a7a3af6e0c9899b4fb967f296e63b6502f405bf5
jhawcw <jhawcw@gmail.com>
2026-05-24 21:15:45 +0800

    feat: add frontend dockerfile

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Start docker network
+
+    docker network create goals-network
+
 ## Starting database
 
     docker run --name mongodb --rm -d -p 27017:27017 mongo
@@ -7,3 +11,9 @@
     docker build -t goals-node .
 
     docker run --name goals-backend --rm -d -p 80:80 goals-node
+
+## Starting frontend
+
+    docker build -t goals-react .
+
+    docker run --name goals-frontend --rm -d -p 3000:3000 goals-react
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
 
-import GoalInput from './components/goals/GoalInput';
-import CourseGoals from './components/goals/CourseGoals';
-import ErrorAlert from './components/UI/ErrorAlert';
+import GoalInput from "./components/goals/GoalInput"
+import CourseGoals from "./components/goals/CourseGoals"
+import ErrorAlert from "./components/UI/ErrorAlert"
 
 function App() {
-  const [loadedGoals, setLoadedGoals] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [loadedGoals, setLoadedGoals] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   useEffect(function () {
     async function fetchData() {
-      setIsLoading(true);
+      setIsLoading(true)
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch("http://backend/goals")
 
-        const resData = await response.json();
+        const resData = await response.json()
 
         if (!response.ok) {
-          throw new Error(resData.message || 'Fetching the goals failed.');
+          throw new Error(resData.message || "Fetching the goals failed.")
         }
 
-        setLoadedGoals(resData.goals);
+        setLoadedGoals(resData.goals)
       } catch (err) {
-        setError(
-          err.message ||
-            'Fetching goals failed - the server responsed with an error.'
-        );
+        setError(err.message || "Fetching goals failed - the server responsed with an error.")
       }
-      setIsLoading(false);
+      setIsLoading(false)
     }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   async function addGoalHandler(goalText) {
-    setIsLoading(true);
+    setIsLoading(true)
 
     try {
-      const response = await fetch('http://localhost/goals', {
-        method: 'POST',
+      const response = await fetch("http://backend/goals", {
+        method: "POST",
         body: JSON.stringify({
           text: goalText,
         }),
         headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+          "Content-Type": "application/json",
+        },
+      })
 
-      const resData = await response.json();
+      const resData = await response.json()
 
       if (!response.ok) {
-        throw new Error(resData.message || 'Adding the goal failed.');
+        throw new Error(resData.message || "Adding the goal failed.")
       }
 
       setLoadedGoals((prevGoals) => {
@@ -62,43 +59,37 @@ function App() {
             text: goalText,
           },
           ...prevGoals,
-        ];
-        return updatedGoals;
-      });
+        ]
+        return updatedGoals
+      })
     } catch (err) {
-      setError(
-        err.message ||
-          'Adding a goal failed - the server responsed with an error.'
-      );
+      setError(err.message || "Adding a goal failed - the server responsed with an error.")
     }
-    setIsLoading(false);
+    setIsLoading(false)
   }
 
   async function deleteGoalHandler(goalId) {
-    setIsLoading(true);
+    setIsLoading(true)
 
     try {
-      const response = await fetch('http://localhost/goals/' + goalId, {
-        method: 'DELETE',
-      });
+      const response = await fetch("http://backend/goals/" + goalId, {
+        method: "DELETE",
+      })
 
-      const resData = await response.json();
+      const resData = await response.json()
 
       if (!response.ok) {
-        throw new Error(resData.message || 'Deleting the goal failed.');
+        throw new Error(resData.message || "Deleting the goal failed.")
       }
 
       setLoadedGoals((prevGoals) => {
-        const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
-        return updatedGoals;
-      });
+        const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId)
+        return updatedGoals
+      })
     } catch (err) {
-      setError(
-        err.message ||
-          'Deleting the goal failed - the server responsed with an error.'
-      );
+      setError(err.message || "Deleting the goal failed - the server responsed with an error.")
     }
-    setIsLoading(false);
+    setIsLoading(false)
   }
 
   return (
@@ -106,10 +97,13 @@ function App() {
       {error && <ErrorAlert errorText={error} />}
       <GoalInput onAddGoal={addGoalHandler} />
       {!isLoading && (
-        <CourseGoals goals={loadedGoals} onDeleteGoal={deleteGoalHandler} />
+        <CourseGoals
+          goals={loadedGoals}
+          onDeleteGoal={deleteGoalHandler}
+        />
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
